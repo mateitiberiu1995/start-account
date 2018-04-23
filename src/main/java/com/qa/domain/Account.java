97cc80a21a643f20f0f28ea83cb39a33b4f4097a
Admin <Admin@STUDENT07>
2018-04-23 15:00:12 +0100

    we evolved

--- src/main/java/com/qa/domain/Account.java
+++ src/main/java/com/qa/domain/Account.java
@@ -1,10 +1,19 @@
 package com.qa.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Account {
 
+	@GeneratedValue @Id
+	private Long id;
+	@Column(length=16)
 	private String firstName;
+	@Column(length=16)
 	private String secondName;
+	@Column(length=6)
 	private String accountNumber;
+	
 
 	public Account(String firstName, String secondName, String accountNumber) {
 		this.firstName = firstName;
