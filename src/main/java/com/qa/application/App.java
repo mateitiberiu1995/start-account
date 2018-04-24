8dffe571a5a9ed32fa815add0087074de54a4bd7
Admin <Admin@STUDENT07>
2018-04-24 17:54:38 +0100

    trying the interface

--- src/main/java/com/qa/application/App.java
+++ src/main/java/com/qa/application/App.java
@@ -10,14 +10,15 @@ import com.qa.util.JSONUtil;
 public class App {
 	
 	public static void main(String[] args) {
-		AccountService service = new AccountService();
+		/*AccountService service = new AccountService();
 		JSONUtil util = new JSONUtil();
 		Account joeBloggs = new Account("Joe", "Bloggs", "1238");
 		Account janeBloggs = new Account("Jane", "Bloggs", "1239");
 		service.addAccountFromMap(joeBloggs);
 		service.addAccountFromMap(janeBloggs);
 		String mapAsJSON = util.getJSONForObject(service.getAccountMap());
-		System.out.println("This is the account map as JSON " + mapAsJSON);
+		System.out.println("This is the account map as JSON " + mapAsJSON);*/
 	}
+		
 
 }
--- src/main/java/com/qa/service/AccountService.java
+++ src/main/java/com/qa/service/AccountService.java
@@ -3,19 +3,66 @@ package com.qa.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.inject.Inject;
+
 import com.qa.domain.Account;
+import com.qa.util.JSONUtil;
 
 public class AccountService {
 
-	private Map<Integer, Account> accountMap;
+	@Inject
+	private Map<Long, Account> accountMap;
 
-	private int count = 0;
+	@Inject
+	private JSONUtil util;
 
-	public AccountService() {
+	/*public AccountService() {
 		accountMap = new HashMap<Integer, Account>();
+	}*/
+	
+	public String getAllAccounts()
+	{
+		return util.getJSONForObject(accountMap);
 	}
-
-	public void addAccountFromMap(Account userAccount) {
+	
+	public String addAccount(String account) {
+		Account anAccount = util.getObjectForJSON(account , Account.class);
+		if(accountMap.containsValue(anAccount))
+		{
+			return "{\"message\": \"account is already in the database\"}";
+		}
+		else
+		{
+		accountMap.put((long) (accountMap.size()+1), anAccount);
+		return "{\"message\": \"account sucessfully added\"}";
+		}
+		
+	}
+	
+	public String updateAccount(Long id, String account) {
+		Account anAccount = util.getObjectForJSON(account, Account.class);
+		if(accountMap.containsKey(id))
+		{
+			accountMap.replace(id, anAccount);
+			return "{\"message\": \"account sucessfully updated\"}";
+		}
+		else
+			return "{\"message\": \"account couldn't be updated\"}";
+		
+	}
+	
+	public String deleteAccount(Long id)
+	{
+		if(accountMap.containsKey(id))
+		{
+			accountMap.remove(id);
+			return "{\"message\": \"account sucessfully removed\"}";
+		}
+		else
+			return "{\"message\": \"account couldn't be removed\"}";
+		
+	}
+	/*public void addAccountFromMap(Account userAccount) {
 		accountMap.put(count, userAccount);
 		
 		count++;
@@ -35,6 +82,6 @@ public class AccountService {
 	public int getNumberOfAccountWithFirstName(String firstNameOfAccount) {
 		return (int) accountMap.values().stream()
 				.filter(eachAccount -> eachAccount.getFirstName().equals(firstNameOfAccount)).count();
-	}
+	}*/
 
 }
